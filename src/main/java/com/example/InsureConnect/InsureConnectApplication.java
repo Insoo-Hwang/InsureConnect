463764a0185433c63bed2b9d051fc2bad4533819
iamin <iaminsoo@naver.com>
2024-01-01 16:30:21 +0900

    Initial commit

---
+++ src/main/java/com/example/InsureConnect/InsureConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.InsureConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsureConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InsureConnect/InsureConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InsureConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsureConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
