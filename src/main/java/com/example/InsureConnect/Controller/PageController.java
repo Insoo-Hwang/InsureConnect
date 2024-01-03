ddc156788c139d4a5e1c7405a3aec62081e03bfa
Machrie <haneol119@naver.com>
2024-01-03 20:46:52 +0900

    main페이지 / devtools

--- src/main/java/com/example/InsureConnect/Controller/PageController.java
+++ src/main/java/com/example/InsureConnect/Controller/PageController.java
@@ -2,7 +2,6 @@ package com.example.InsureConnect.Controller;
 
 import com.example.InsureConnect.Service.KakaoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +12,15 @@ public class PageController {
 
     private final KakaoService kakaoService;
 
-    @GetMapping("/")
-    public String login(Model model){
+    @GetMapping("/login")
+    public String login(Model model) {
         model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
 
         return "login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
     }
 }
