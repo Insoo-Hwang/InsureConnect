fc05e20d28e6e26a6561efc5775f311ce805f581
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2024-01-19 23:24:27 +0900

    API키 제거

--- src/main/java/com/example/InsureConnect/Config/ChatGptConfig.java
+++ src/main/java/com/example/InsureConnect/Config/ChatGptConfig.java
@@ -3,7 +3,7 @@ package com.example.InsureConnect.Config;
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "{YOUR_API_KEY}";
     public static final String MODEL = "gpt-3.5-turbo-instruct";
     public static final Integer MAX_TOKEN = 1000;
     public static final Double TEMPERATURE = 0.0;
